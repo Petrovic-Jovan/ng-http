4ac52a1cfdef1408a53eb1347d7d5b41227bf6b0
jovan.petrovic <jovanloki@gmail.com>
2026-03-25 10:08:44 +0100

    Using some code duplication to make favorite places component to work.

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-places',
@@ -11,4 +14,46 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  isLoading = signal(true);
+  // error signal, initial value is an empty string
+  error = signal('');
+
+  // On initialzation of the component, we fetch the available places from the backend
+  ngOnInit() {
+    this.isLoading.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching your favorite places. Please try again later.',
+              ),
+          );
+        }),
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isLoading.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
